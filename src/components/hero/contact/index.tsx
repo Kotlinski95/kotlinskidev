3e151e4fa4aab4085e804803ba21e8515cd96af4
Adrian Kotlinski <adrian.kotlinski@script-studio.pl>
2021-05-21 20:03:29 +0200

    add contact component to homepage hero

---
+++ src/components/hero/contact/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+import ContactForm from '../../contactForm'
+const ContactComponent = () => {
+    return (
+      <div className="hero-contact-wrapper">
+          <ContactForm/>
+      </div>
+    )
+  };
+
+export default ContactComponent;
