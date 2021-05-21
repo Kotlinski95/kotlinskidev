66d9f8e9c543ba75726b44760d77e58bbbd2f8d9
Adrian Kotlinski <adrian.kotlinski@script-studio.pl>
2021-05-21 22:58:59 +0200

    Added Front-end stack page

--- src/pages/stack/front_end_developer.tsx
+++ src/pages/stack/front_end_developer.tsx
@@ -2,6 +2,15 @@ import TypedText from '../../components/typedText'
 import {setPage} from '../../reducers/state'
 import {useDispatch } from 'react-redux'
 import Breadcrumbs from '../../components/breadcrumbs'
+import StackTile from '../../components/stackTile'
+import html from '../../assets/stack/html.svg'
+import css from '../../assets/stack/css.svg'
+import js from '../../assets/stack/javascript.svg'
+import sass from '../../assets/stack/sass.svg'
+import ts from '../../assets/stack/typescript.svg'
+import react from '../../assets/stack/react.svg'
+import nodejs from '../../assets/stack/nodejs.svg'
+import github from '../../assets/stack/github.svg'
 
 const FrontEndDeveloperPage = () => {
   const dispatch = useDispatch();
@@ -9,7 +18,17 @@ const FrontEndDeveloperPage = () => {
   return(
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%'}}>
       <Breadcrumbs />
-      <TypedText />
+      <h2 className="stack-hello">{language.pages.stack.front_header}</h2>
+      <div className="stack-wrapper">
+        <StackTile logo={html} width="150px" heigh="150px" title="HTML" imgTitle="HTML 5"/>
+        <StackTile logo={css} width="150px" heigh="150px" title="CSS" imgTitle="CSS 3"/>
+        <StackTile logo={js} width="150px" heigh="150px" title="JAVASCRIPT" imgTitle="JavaScript"/>
+        <StackTile logo={sass} width="150px" heigh="150px" title="SASS" imgTitle="Sass"/>
+        <StackTile logo={ts} width="150px" heigh="150px" title="TYPESCRIPT" imgTitle="TypeScript"/>
+        <StackTile logo={react} width="150px" heigh="150px" title="REACT" imgTitle="React"/>
+        <StackTile logo={nodejs} width="150px" heigh="150px" title="NODE JS" imgTitle="Node JS"/>
+        <StackTile logo={github} width="150px" heigh="150px" title="GIT" imgTitle="Github"/>
+      </div>
     </div>
   );
 };
