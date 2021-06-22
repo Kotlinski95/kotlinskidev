47d31fab28885f503ed7b3d175ad427922a4b673
Adrian Kotlinski <adrian.kotlinski@rb.com>
2021-06-22 17:41:32 +0200

    Added logo in welcome hero page

--- src/components/hero/welcome/index.tsx
+++ src/components/hero/welcome/index.tsx
@@ -26,8 +26,8 @@ const WelcomeComponent = (props) => {
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', position: 'relative' }}>
                 <div className="welcome-wrapper" data-scroll>
                     <div className="introduce-text cursor_hover">
-                    <TextRevealVertical delay={.2} data-scroll><h1>{language.pages.home.welcome.header}</h1></TextRevealVertical>
-                    <TextRevealVertical delay={1.2} data-scroll><h2>{language.pages.home.welcome.title}</h2></TextRevealVertical>
+                    {/* <TextRevealVertical delay={.2} data-scroll><h1>{language.pages.home.welcome.header}</h1></TextRevealVertical>
+                    <TextRevealVertical delay={1.2} data-scroll><h2>{language.pages.home.welcome.title}</h2></TextRevealVertical> */}
                     </div>
                 </div>
                 <ScrollDown data-scroll bottom="35px" top="auto" bottomText="-10px" topText="auto" display={showScroll} text={language.pages.home.welcome.scroll} />
